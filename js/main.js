5de35073b251ba2f7e41186c6b7d2c37f75a33aa
slava3113 <slava3113@ramler.ru>
2022-05-10 10:57:51 +0600

    slider

---
+++ js/main.js
@@ -0,0 +1,13 @@
+ $(document).ready(function () {
+   $('.reviews__slider').slick({
+    centerMode: true,
+    centerPadding: '45px',
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    centerMode: true,
+    focusOnSelect: true,
+    dots: false,
+    
+   });
+
+ });
